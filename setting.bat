dca402d664d56523e5364de452ef5d0759164af3
阿拉丁 <2970136728@qq.com>
2025-02-09 16:55:27 +0800

    test

---
+++ setting.bat
@@ -0,0 +1,8 @@
+@REM 安装ts环境
+@REM 快速创建带有默认设置的 package.json 文件。
+@REM npm init -y
+@REM 下载基础库，npm i == npm install
+@REM npm i -d typescript
+@REM npm i -d typescript-to-lua
+@REM npm install -D tslib @types/node   该命令是安装TypeScript助手的运行时库，包含所有TypeScript辅助函数
+npm init -y && npm i -d typescript && npm i -d typescript-to-lua
