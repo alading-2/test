5902a2517e0cec3b041dc990884de8166af1e85e
阿拉丁 <2970136728@qq.com>
2025-02-09 17:09:14 +0800

    test

--- .history/TStoLuaTest/test_20250112202303.ts
+++ .history/TStoLuaTest/test_20250112202303.ts
@@ -1,11 +1,10 @@
-/**   @noselfinfile  **/
+ /**   @noselfinfile  **/ 
 export function add(a: number, b: number) {
     return a + b
 }
 
-let array = [11, 2, 22, 1]
-array.sort((a, b) => b + a)
+let array=[11,2,22,1]
+array.sort((a, b) => b+a)
 for (const element of array) {
     console.log(element)
 }
-
